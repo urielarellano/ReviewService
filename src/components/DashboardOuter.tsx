54537da8702fec6567b66a1039afe8bd3f48b5fe
urielarellano <urielarellano017@gmail.com>
2025-09-22 10:34:56 -0500

    fixed merge error

--- src/components/DashboardOuter.tsx
+++ src/components/DashboardOuter.tsx
@@ -21,19 +21,6 @@ function DashboardOuter({ view, setOuterView }: OuterProps) {
     const requestCopyRef = useRef<HTMLDivElement | null>(null);
     const location = window.location.href;
 
-    const review: Review = {
-        uid: "",
-        id: "",
-        approved: false,
-        image: floria,
-        name: "Floria Elizabeth",
-        bizName: "Floria Flowers",
-        service: "Marketing Funnel Optimization",
-        stars: 5,
-        testimonial: "I cannot say enough good things about Arellano Advertising! They helped my flower business go from a smaller local store to a well-known regional flower business. The results spoke for themselves and the sales they produced more than justified the price. Highly recommended!",
-        createdAt: new Date()
-    }
-
     // set requestUrl and uid
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
